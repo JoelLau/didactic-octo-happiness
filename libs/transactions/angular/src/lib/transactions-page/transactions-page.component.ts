9dc32107c14a331c5483d7df7cef592bd177975c
Joel Lau <joel.lau@protonmail.com>
2022-12-19 14:20:35 +0800

    wip

--- libs/transactions/angular/src/lib/transactions-page/transactions-page.component.ts
+++ libs/transactions/angular/src/lib/transactions-page/transactions-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { read, utils } from 'xlsx';
 import { TRANSACTIONS_PAGE_TEST_IDS } from './testing';
 
 @Component({
@@ -6,4 +7,49 @@ import { TRANSACTIONS_PAGE_TEST_IDS } from './testing';
   templateUrl: './transactions-page.component.html',
   styleUrls: ['./transactions-page.component.scss'],
 })
-export class TransactionsPageComponent {}
+export class TransactionsPageComponent {
+  readonly TEST_IDS = TRANSACTIONS_PAGE_TEST_IDS;
+
+  async onFileChange(event: Event) {
+    const eventTarget = event.target as HTMLInputElement;
+    const files: FileList | null = eventTarget.files;
+    if (files === null || files.length < 1) {
+      return;
+    }
+
+    Array.from(files).forEach(async (file) => {
+      const ws = read(await file.arrayBuffer());
+      Object.values(ws.Sheets).forEach((sheet) => {
+        const delimeter = '\n';
+        const csv = utils
+          .sheet_to_csv(sheet, { RS: delimeter })
+          .split(delimeter);
+
+        console.log(`isOcbcAccSheet: ${this.isOcbcAccSheet(csv)}`);
+      });
+    });
+  }
+
+  private isOcbcAccSheet(csv: string[]): boolean {
+    if (csv.length < 5) {
+      return false;
+    }
+
+    if (!csv[0].startsWith(`Account details for:`)) {
+      console.log("doesn't start with account details for");
+      return false;
+    }
+
+    if (!csv[1].startsWith(`Available Balance`)) {
+      console.log("doesn't start with available balance");
+      return false;
+    }
+
+    if (!csv[2].startsWith(`Ledger Balance`)) {
+      console.log("doesn't start with ledger balance");
+      return false;
+    }
+
+    return true;
+  }
+}
--- libs/transactions/angular/src/lib/transactions-page/transactions-page.module.ts
+++ libs/transactions/angular/src/lib/transactions-page/transactions-page.module.ts
@@ -1,7 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TransactionsPageComponent } from './transactions-page.component';
 
 @NgModule({
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   declarations: [TransactionsPageComponent],
   exports: [TransactionsPageComponent],
 })
